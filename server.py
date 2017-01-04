5d0edef2725468d27384b25446716e69f4ff0e77
RAJKUMAR NATARAJAN <rajkumna@in.ibm.com>
2017-01-04 06:12:01 -0600

    tyughij

--- server.py
+++ server.py
@@ -1,75 +1,11 @@
 import os
 import pymongo
 # import ssl
-import json
-from pymongo import MongoClient
 
-try:
-    from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-    from SocketServer import TCPServer as Server
-except ImportError:
-    from http.server import SimpleHTTPRequestHandler as Handler
-    from http.server import HTTPServer as Server
+MONGODB_URL = os.environ.get('mongodb://admin:PPOETEQMMQUSMJRG@bluemix-sandbox-dal-9-portal.4.dblayer.com:21452/admin?ssl=true')
+client = pymongo.MongoClient(MONGODB_URL,ssl_ca_certs="mongpyt.crt")
+db = client.get_default_database()
+print db.collection_names() 
 
-print("hai   1")
-print("----------------------------------")
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
-print("hai   2")
-print("----------------------------------")
 
 # VCAP_SERVICES mapping Start
-
-services = os.getenv('VCAP_SERVICES')
-services_json = json.loads(services)
-mongodb_url = services_json['compose-for-mongodb'][0]['credentials']['uri']
-
-print("hai   3")
-print("----------------------------------")
-# connect:
-client = MongoClient(mongodb_url)
-print("hai   4")
-print("----------------------------------")
-# get the default database:
-db = client.get_default_database()
-print('connected to mongodb!, welcome to mongodb connection, have a fun')
-print db
-print("----------------------------------")
-print('############# CCODE RUN FROM HERE ################')
-db = client['test-database']
-db = client.test_database
-collection = db['test-collection']
-print db
-post = {"author": "Mike", "text": "My first blog post!", "tags": ["mongodb", "python", "pymongo"]}
-collection.insert_one({"author":"MIKE"})
-
-
-
-print("----------------------------------")
-print('Now accesed succesfullty')
-
-print("----------------------------------")
-#sse=db.collection_names()
-#print sse
-#posts = db.posts
-#posts.insert(post)
-#cc=posts.find_one({"author": "Mike"})
-#for reget in cc:
-#	print reget
-print('############# CCODE completed HERE ################')	
-
-
-httpd = Server(("", PORT), Handler)
-try:
-    print("Start serving at port %i" % PORT)
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    pass
-httpd.server_close()
-
-print("hai   5")
-print("----------------------------------")
-
-print("-------------------OVER-----------------------")
