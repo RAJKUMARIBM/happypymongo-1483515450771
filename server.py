3f9c97bfdb38aae2088f541882758ab8fabfeb73
RAJKUMAR NATARAJAN <rajkumna@in.ibm.com>
2017-01-04 02:03:05 -0600

    pleaserun

--- server.py
+++ server.py
@@ -1,48 +1,50 @@
 import os
 import pymongo
-import json  
+# import ssl
+import json
 from pymongo import MongoClient
+
 try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
+    from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+    from SocketServer import TCPServer as Server
 except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+    from http.server import SimpleHTTPRequestHandler as Handler
+    from http.server import HTTPServer as Server
 
-
-print ("HAI 1")
-print ("################################################")
+print("hai   1")
+print("----------------------------------")
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('PORT', 8000))
 # Change current directory to avoid exposure of control files
 os.chdir('static')
-print ("HAI 2")
-print ("################################################")
+print("hai   2")
+print("----------------------------------")
+
 # VCAP_SERVICES mapping Start
 
 services = os.getenv('VCAP_SERVICES')
 services_json = json.loads(services)
 mongodb_url = services_json['compose-for-mongodb'][0]['credentials']['uri']
-print ("HAI 3")
-print ("################################################")
-#connect:
-client = MongoClient(mongodb_url)  
-#get the default database:
-db = client.get_default_database()  
+print("hai   3")
+print("----------------------------------")
+# connect:
+client = MongoClient(mongodb_url)
+print("hai   4")
+print("----------------------------------")
+# get the default database:
+db = client.get_default_database()
 print('connected to mongodb!, welcome to mongodb connection, have a fun')
 print db
-print ("HAI 4")
-print ("################################################")
-# VCAP_SERVICES mapping END
+print("----------------------------------")
 
-print ("HAI 5")
-print ("################################################")
+
 httpd = Server(("", PORT), Handler)
 try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
+    print("Start serving at port %i" % PORT)
+    httpd.serve_forever()
 except KeyboardInterrupt:
-  pass
+    pass
 httpd.server_close()
-print ("HAI 6")
-print ("################################################")
+
+print("hai   5")
+print("----------------------------------")
