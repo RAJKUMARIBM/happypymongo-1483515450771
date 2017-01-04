2fdfef999a2a1d1382081057dc561473284c1673
RAJKUMAR NATARAJAN <rajkumna@in.ibm.com>
2017-01-04 06:43:03 -0600

    SDfsd

--- server.py
+++ server.py
@@ -1,7 +1,56 @@
 import os
 import pymongo
 # import ssl
-MONGODB_URL = "mongodb://admin:VSBVBFCGCIFGXQFS@bluemix-sandbox-dal-9-portal.0.dblayer.com:19651/admin?ssl=true"
-client = pymongo.MongoClient(MONGODB_URL,ssl_ca_certs="Mongo.crt")
-#db = client.get_default_database()
+import json
+from pymongo import MongoClient
+
+try:
+    from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+    from SocketServer import TCPServer as Server
+except ImportError:
+    from http.server import SimpleHTTPRequestHandler as Handler
+    from http.server import HTTPServer as Server
+
+print("hai   1")
+print("----------------------------------")
+# Read port selected by the cloud for our application
+PORT = int(os.getenv('PORT', 8000))
+# Change current directory to avoid exposure of control files
+os.chdir('static')
+print("hai   2")
+print("----------------------------------")
+
+# VCAP_SERVICES mapping Start
+
+services = os.getenv('VCAP_SERVICES')
+services_json = json.loads(services)
+mongodb_url = services_json['compose-for-mongodb'][0]['credentials']['uri']
+print("hai   3")
+print("----------------------------------")
+# connect:
+client = MongoClient(mongodb_url)
+print("hai   4")
+print("----------------------------------")
+# get the default database:
+db = client.get_default_database()
+print('connected to mongodb!, welcome to mongodb connection, have a fun')
+print db
+print("----------------------------------")
+
+
+
+print("hai   5")
+print("----------------------------------")
+print client.database_names()
+
+httpd = Server(("", PORT), Handler)
+try:
+    print("Start serving at port %i" % PORT)
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+httpd.server_close()
+
+print("hai   5")
+print("----------------------------------")
 print client.database_names()
