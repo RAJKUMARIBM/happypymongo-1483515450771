89049f65dd8d690bfcc9d9dfab10884ecc80a61a
RAJKUMAR NATARAJAN <rajkumna@in.ibm.com>
2017-01-05 03:22:24 -0600

    safgsagf

--- server.py
+++ server.py
@@ -1,50 +1,41 @@
 import os
 import pymongo
-# import ssl
+#import ssl
 import json
 from pymongo import MongoClient
-
 try:
-    from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-    from SocketServer import TCPServer as Server
+  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+  from SocketServer import TCPServer as Server
 except ImportError:
-    from http.server import SimpleHTTPRequestHandler as Handler
-    from http.server import HTTPServer as Server
+  from http.server import SimpleHTTPRequestHandler as Handler
+  from http.server import HTTPServer as Server
 
-print("hai   1")
-print("----------------------------------")
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('PORT', 8000))
 # Change current directory to avoid exposure of control files
 os.chdir('static')
-print("hai   2")
-print("----------------------------------")
 
 # VCAP_SERVICES mapping Start
 
 services = os.getenv('VCAP_SERVICES')
 services_json = json.loads(services)
 mongodb_url = services_json['compose-for-mongodb'][0]['credentials']['uri']
-print("hai   3")
-print("----------------------------------")
-# connect:
+#connect:
 client = MongoClient(mongodb_url)
-print("hai   4")
-print("----------------------------------")
-# get the default database:
+#get the default database:
 db = client.get_default_database()
 print('connected to mongodb!, welcome to mongodb connection, have a fun')
 print db
+db.web_cont.insert_one({"content": "", "f_flag": "y", "relevent_url_flag": "y", "base_url_flag": "y","ilock":"y"})
+
 print("----------------------------------")
 
+# VCAP_SERVICES mapping END
 
 httpd = Server(("", PORT), Handler)
 try:
-    print("Start serving at port %i" % PORT)
-    httpd.serve_forever()
+  print("Start serving at port %i" % PORT)
+  httpd.serve_forever()
 except KeyboardInterrupt:
-    pass
+  pass
 httpd.server_close()
-
-print("hai   5")
-print("----------------------------------")
